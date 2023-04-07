d79e7db1a53b72f59ff8eedd3f9a4102ebabea3b
Angelo Carasig <s3906344@student.rmit.edu.au>
2023-04-08 02:14:09 +1000

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Terminus';
+}
