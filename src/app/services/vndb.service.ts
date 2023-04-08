af0a3b60613a79a841594400bc2c9223c77f9726
angelocarasig <angelocarasig15@gmail.com>
2023-04-08 21:37:34 +1000

    TODO: auth/login validation and tracking of userdetails

---
+++ src/app/services/vndb.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VndbService {
+
+  constructor(private http: HttpClient) { }
+
+  getUser()
+
+  getAuthInfo(authKey: string): Observable<any> {
+    const headers = new HttpHeaders().set('Authorization', `token ${authKey}`);
+    return this.http.get('https://api.vndb.org/kana/authinfo', { headers });
+  }
+}
