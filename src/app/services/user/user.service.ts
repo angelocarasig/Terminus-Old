ae0c398763864e0623aba12d9d5fff444c4c5235
angelocarasig <angelocarasig15@gmail.com>
2023-05-05 22:36:23 +1000

    Use behaviourSubjet for userService

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,29 +2,28 @@ import { Injectable } from '@angular/core';
 import { User } from './../../shared/models/User';
 import { environment } from 'src/environments/environment.prod';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 import { USER_PERMISSIONS } from 'src/app/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private currentUser: User | null = null;
+  private currentUser$ = new BehaviorSubject<User | null>(null);
 
   constructor(private http: HttpClient) {}
 
   public async verifyUser(userToVerify: User): Promise<boolean> {
     const url = `${environment.apiUrl}${environment.endpoints.user}`;
-    const params = new HttpParams()
-    .append('q', userToVerify.username);
-
-    let valid: boolean = true;
+    const params = new HttpParams().append('q', userToVerify.username);
+    let valid = true;
 
     try {
-      const response: any = await this.http.get(url, {params}).toPromise();
-      valid = response[`${userToVerify.username}`] != null;
+      const response: any = await this.http.get(url, { params }).toPromise();
+      valid = response[userToVerify.username] != null;
 
       if (valid) {
-        userToVerify.uid = parseInt(response[`${userToVerify.username}`].id.substring(1), 10);
+        userToVerify.uid = parseInt(response[userToVerify.username].id.substring(1), 10);
       }
 
     } catch (error) {
@@ -33,46 +32,46 @@ export class UserService {
     }
 
     return valid;
-}
-
-public async verifyToken(userTokenToVerify: User): Promise<boolean> {
-  const url = `${environment.apiUrl}${environment.endpoints.authinfo}`;
-  const headers = new HttpHeaders().set('Authorization', `token ${userTokenToVerify.apiToken}`);
-
-  let valid: boolean = true;
-
-  try {
-    const response: any = await this.http.get(url, { headers }).toPromise();
-    
-    for (let key in response) {
-      if (response.hasOwnProperty(key)) {
-        valid = this._verifyTokenResponseKeys(userTokenToVerify, key, response[key]);
-      }
-      else valid = false;
-    }
-  } catch (error) {
-    console.error(error);
-    valid = false;
   }
 
-  return valid;
-}
+  public async verifyToken(userTokenToVerify: User): Promise<boolean> {
+    const url = `${environment.apiUrl}${environment.endpoints.authinfo}`;
+    const headers = new HttpHeaders().set('Authorization', `token ${userTokenToVerify.apiToken}`);
+    let valid = true;
 
-  private _verifyTokenResponseKeys(userTokenToVerify: User, responseKey: string, reponseValue: any,): boolean {
-    let valid: boolean = true;
+    try {
+      const response: any = await this.http.get(url, { headers }).toPromise();
+
+      for (let key in response) {
+        if (response.hasOwnProperty(key)) {
+          valid = this._verifyTokenResponseKeys(userTokenToVerify, key, response[key]);
+        } else {
+          valid = false;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      valid = false;
+    }
+
+    return valid;
+  }
+
+  private _verifyTokenResponseKeys(userTokenToVerify: User, responseKey: string, responseValue: any): boolean {
+    let valid = true;
     switch (responseKey) {
       case 'permissions':
-        reponseValue.forEach((permission: string) => {
+        responseValue.forEach((permission: string) => {
           valid = USER_PERMISSIONS.includes(permission);
         });
         break;
 
       case 'username':
-        valid = reponseValue === userTokenToVerify.username;
+        valid = responseValue === userTokenToVerify.username;
         break;
 
       case 'id':
-        valid = reponseValue === `u${userTokenToVerify.uid}`;
+        valid = responseValue === `u${userTokenToVerify.uid}`;
         break;
 
       default:
@@ -82,10 +81,10 @@ public async verifyToken(userTokenToVerify: User): Promise<boolean> {
   }
 
   public setCurrentUser(user: User): void {
-    this.currentUser = user;
+    this.currentUser$.next(user);
   }
 
-  public getCurrentUser(): User | null {
-    return this.currentUser;
+  public getCurrentUser$() {
+    return this.currentUser$.asObservable();
   }
 }
