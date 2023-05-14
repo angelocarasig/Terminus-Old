370a1910fd5907ca4c8e82d88b3ca926ce6951e5
Kyokan7 <acarasig07@gmail.com>
2023-05-14 23:45:37 +1000

    small resizing changes

--- src/app/components/bookshelf/bookshelf.component.ts
+++ src/app/components/bookshelf/bookshelf.component.ts
@@ -2,21 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { SAMPLE_NOVEL } from 'src/app/constants';
 import { UserService } from 'src/app/services/user/user.service';
 import { User } from 'src/app/shared/models/User';
+import { UserNovel } from 'src/app/shared/models/UserNovel';
 
 @Component({
   selector: 'app-bookshelf',
   templateUrl: './bookshelf.component.html',
   styleUrls: ['./bookshelf.component.scss']
 })
-export class BookshelfComponent implements OnInit {  
+export class BookshelfComponent implements OnInit {
   public currentUser: User | null = null;
-  userNovels = SAMPLE_NOVEL;
-  
+  userNovels: UserNovel[] | null | undefined = SAMPLE_NOVEL;
+
   constructor(private userService: UserService) {};
 
   ngOnInit(): void {
     this.userService.getCurrentUser$().subscribe(user => {
       this.currentUser = user;
+      this.userNovels = this.currentUser?.ulist;
     });
   }
 }
