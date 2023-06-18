ecc544cdb605f0d80596ef99772c0c6b0f82b9e4
angelocarasig <angelocarasig15@gmail.com>
2023-06-18 16:09:57 +1000

    feat: R18 Filter

--- src/app/components/book-card/book-card.component.ts
+++ src/app/components/book-card/book-card.component.ts
@@ -1,13 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { UserNovel } from 'src/app/shared/models/UserNovel';
+import { SettingsService } from 'src/app/services/settings/settings.service';
+import { ImageSensitivity, Settings } from 'src/app/shared/models/Settings';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
   styleUrls: ['./book-card.component.scss']
 })
-export class BookCardComponent {
+export class BookCardComponent implements OnInit {
   @Input() novel: UserNovel;
+
+  settings: Settings;
+
+  constructor(private settingsService: SettingsService) {}
+
+  ngOnInit(): void {
+    this.settingsService.getOptions().subscribe(settings => {
+      this.settings = settings;
+    });
+  }
 
   getBackgroundUrl(novel: UserNovel): string {
     return `url(${novel.vn.image.url})`;
@@ -15,5 +27,29 @@ export class BookCardComponent {
 
   onClick(): void {
     console.log(this.novel);
+  }
+
+  verifyBlurEffect(): boolean {
+    let shouldBlur: boolean;
+
+    switch (this.settings.imageSensitivity) {
+      case ImageSensitivity.Safe:
+        shouldBlur = this.novel.vn.image.sexual > 0;
+        break;
+
+      case ImageSensitivity.Questionable:
+        shouldBlur = this.novel.vn.image.sexual > 1;
+        break;
+
+      case ImageSensitivity.Explicit:
+        shouldBlur = false;
+        break;
+
+      default:
+        shouldBlur = false;
+        break;
+      }
+    
+    return shouldBlur;
   }
 }
--- src/app/shared/models/Settings.ts
+++ src/app/shared/models/Settings.ts
@@ -3,3 +3,18 @@ export class SettingOption {
     public static readonly Novel = 'Novel';
     public static readonly User = 'User';
 }
+
+//TODO: Refactor to enum
+export class ImageSensitivity {
+    public static readonly Safe = 0;
+    public static readonly Questionable = 1;
+    public static readonly Explicit = 2;
+}
+
+export class Settings {
+    imageSensitivity: ImageSensitivity;
+
+    constructor() {
+        this.imageSensitivity = ImageSensitivity.Safe;
+    }
+}
