604e0a4fde3cd77871c63f2c716c6917c0868bad
korewashiroi <angelocarasig15@gmail.com>
2023-06-23 23:27:04 +1000

    chore: Save settings to local storage (#16)

--- src/app/services/settings/settings.service.ts
+++ src/app/services/settings/settings.service.ts
@@ -9,7 +9,10 @@ export class SettingsService {
 
   private settings = new BehaviorSubject<Settings>(new Settings());
 
-  constructor() {}
+  constructor() {
+    const settingsFromLocal = localStorage.getItem('settings');
+    this.settings.next(settingsFromLocal != null ? JSON.parse(settingsFromLocal) : new Settings());
+  }
 
   getOptions(): BehaviorSubject<Settings> {
     return this.settings;
@@ -18,5 +21,6 @@ export class SettingsService {
   toggleImageSensitivity(updatedImageSensitivity: ImageSensitivity) {
     this.settings.value.imageSensitivity = updatedImageSensitivity;
     this.settings.next(this.settings.value);
+    localStorage.setItem('settings', JSON.stringify(this.settings.value));
   }
 }
